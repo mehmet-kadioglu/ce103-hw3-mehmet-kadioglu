7c6a8d863600424690822d3d68451dfec669270a
unknown <mehmetkadioglu7@gmail.com>
2021-11-23 22:19:51 +0300

    definitons added

--- DepartmentStoreManagementSystem/message.h
+++ DepartmentStoreManagementSystem/message.h
@@ -1,1 +1,98 @@
+/**
+ * @file message.h
+ * @author Mehmet Kadioglu
+ * @date 23 November 2021
+ *
+ * @brief <b> Defining Messages for IDs </b>
+ *
+ * HW3 IDs header
+ *
+ * @see https://www.tutorialspoint.com/cprogramming/c_preprocessors.htm
+ */
+
 #pragma once
+
+ /*messages are numbered with unique ID and this message
+ translated text are stored on en.dat and tr.dat files*/
+#define translation0 0
+#define translation1 1
+#define translation2 2
+#define translation3 3
+#define translation4 4
+#define translation5 5
+#define translation6 6
+#define translation7 7
+#define translation8 8
+#define translation9 9
+#define translation10 10
+#define translation11 11
+#define translation12 12
+#define translation13 13
+#define translation14 14
+#define translation15 15
+#define translation16 16
+#define translation17 17
+#define translation18 18
+#define translation19 19
+#define translation20 20
+#define translation21 21
+#define translation22 22
+#define translation23 23
+#define translation24 24
+#define translation25 25
+#define translation26 26
+#define translation27 27
+#define translation28 28
+#define translation29 29
+#define translation30 30
+#define translation31 31
+#define translation32 32
+#define translation33 33
+#define translation34 34
+#define translation35 35
+#define translation36 36
+#define translation37 37
+#define translation38 38
+#define translation39 39
+#define translation40 40
+#define translation41 41
+#define translation42 42
+#define translation43 43
+#define translation44 44
+#define translation45 45
+#define translation46 46
+#define translation47 47
+#define translation48 48
+#define translation49 49
+#define translation50 50
+#define translation51 51
+#define translation52 52
+#define translation53 53
+#define translation54 54
+#define translation55 55
+#define translation56 56
+#define translation57 57
+#define translation58 58
+#define translation59 59
+#define translation60 60
+#define translation61 61
+#define translation62 62
+#define translation63 63
+#define translation64 64
+#define translation65 65
+#define translation66 66
+#define translation67 67
+#define translation68 68
+#define translation69 69
+#define translation70 70
+#define translation71 71
+#define translation72 72
+#define translation73 73
+#define translation74 74
+#define translation75 75
+#define translation76 76
+#define translation77 77
+#define translation78 78
+#define translation79 79
+#define translation80 80
+#define translation81 81
